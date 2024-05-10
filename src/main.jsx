bf919bda6ae405bb9d927a3773494b7419b958de
Randy H. <>
2024-05-10 19:31:47 +0700

    Apply primary color theme and css baseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "./App.jsx";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#006633",
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 );
