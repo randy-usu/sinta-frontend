540173a38ec68d05863c84a39b04fe2ba4f3900f
Randy H. <>
2024-06-09 10:15:40 +0700

    Add mahasiswa and dosen text/label to auth form

--- src/features/users/sign-in/components/sign-in-form/mahasiswa-sign-in-form.jsx
+++ src/features/users/sign-in/components/sign-in-form/mahasiswa-sign-in-form.jsx
@@ -1,42 +1,52 @@
-import { Box, Button, Container, Link, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 export const MahasiswaSignInForm = () => (
-  <Box component="form" noValidate sx={{ textAlign: "center" }}>
-    <TextField
-      margin="dense"
-      size="small"
-      required
-      fullWidth
-      id="nim"
-      label="NIM"
-      name="nim"
-      autoFocus
-    />
-    <TextField
-      margin="dense"
-      size="small"
-      required
-      fullWidth
-      name="password"
-      label="Kata Sandi"
-      type="password"
-      id="password"
-      autoComplete="current-password"
-    />
-    <Button
-      type="submit"
-      variant="contained"
-      sx={{ mt: 3 }}
-      component={RouterLink}
-      to="sit-in"
-    >
-      Masuk
-    </Button>
-    <Container sx={{ mt: 2 }}>
-      <Link component={RouterLink} to="sign-up">
-        Buat Akun
-      </Link>
-    </Container>
-  </Box>
+  <>
+    <Typography>Mahasiswa</Typography>
+    <Box component="form" noValidate sx={{ textAlign: "center" }}>
+      <TextField
+        margin="dense"
+        size="small"
+        required
+        fullWidth
+        id="nim"
+        label="NIM"
+        name="nim"
+        autoFocus
+      />
+      <TextField
+        margin="dense"
+        size="small"
+        required
+        fullWidth
+        name="password"
+        label="Kata Sandi"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ mt: 3 }}
+        component={RouterLink}
+        to="sit-in"
+      >
+        Masuk
+      </Button>
+      <Container sx={{ mt: 2 }}>
+        <Link component={RouterLink} to="sign-up">
+          Buat Akun
+        </Link>
+      </Container>
+    </Box>
+  </>
 );
--- src/features/users/sign-up/components/sign-up-form/dosen-sign-up-form.jsx
+++ src/features/users/sign-up/components/sign-up-form/dosen-sign-up-form.jsx
@@ -1,59 +1,62 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 export const DosenSignUpForm = () => (
-  <Box component="form" noValidate sx={{ textAlign: "center" }}>
-    <TextField
-      margin="dense"
-      size="small"
-      required
-      fullWidth
-      id="name"
-      label="Nama Lengkap"
-      name="name"
-      autoComplete="name"
-      autoFocus
-    />
-    <TextField
-      margin="dense"
-      size="small"
-      required
-      fullWidth
-      id="email"
-      label="Alamat Surel"
-      name="email"
-      autoComplete="email"
-    />
-    <TextField
-      margin="dense"
-      size="small"
-      required
-      fullWidth
-      name="password"
-      label="Kata Sandi"
-      type="password"
-      id="password"
-      autoComplete="new-password"
-    />
-    <TextField
-      margin="dense"
-      size="small"
-      required
-      fullWidth
-      name="password-confirmation"
-      label="Konfirmasi Kata Sandi"
-      type="password"
-      id="password-confirmation"
-      autoComplete="new-password"
-    />
-    <Button
-      type="submit"
-      variant="contained"
-      sx={{ mt: 3 }}
-      component={RouterLink}
-      to="/dosen/dashboard"
-    >
-      Daftar
-    </Button>
-  </Box>
+  <>
+    <Typography>Dosen</Typography>
+    <Box component="form" noValidate sx={{ textAlign: "center" }}>
+      <TextField
+        margin="dense"
+        size="small"
+        required
+        fullWidth
+        id="name"
+        label="Nama Lengkap"
+        name="name"
+        autoComplete="name"
+        autoFocus
+      />
+      <TextField
+        margin="dense"
+        size="small"
+        required
+        fullWidth
+        id="email"
+        label="Alamat Surel"
+        name="email"
+        autoComplete="email"
+      />
+      <TextField
+        margin="dense"
+        size="small"
+        required
+        fullWidth
+        name="password"
+        label="Kata Sandi"
+        type="password"
+        id="password"
+        autoComplete="new-password"
+      />
+      <TextField
+        margin="dense"
+        size="small"
+        required
+        fullWidth
+        name="password-confirmation"
+        label="Konfirmasi Kata Sandi"
+        type="password"
+        id="password-confirmation"
+        autoComplete="new-password"
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ mt: 3 }}
+        component={RouterLink}
+        to="/dosen/dashboard"
+      >
+        Daftar
+      </Button>
+    </Box>
+  </>
 );
