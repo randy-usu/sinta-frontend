0c015b1e588f30035e2bbb13125271bee599adb6
Randy H. <>
2024-07-21 06:20:52 +0700

    Implement mahasiswa sign up

--- src/features/users/sign-up/components/sign-up-form/mahasiswa-sign-up-form.jsx
+++ src/features/users/sign-up/components/sign-up-form/mahasiswa-sign-up-form.jsx
@@ -1,66 +1,94 @@
-import { Box, Button, Container, Link, TextField, Typography } from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
+import { LoadingButton } from "@mui/lab";
+import { Box, Container, Link, TextField, Typography } from "@mui/material";
+import useAxios from "axios-hooks";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 
-export const MahasiswaSignUpForm = () => (
-  <>
-    <Typography>Mahasiswa</Typography>
-    <Box component="form" noValidate sx={{ textAlign: "center" }}>
-      <TextField
-        margin="dense"
-        size="small"
-        required
-        fullWidth
-        id="name"
-        label="Nama Lengkap"
-        name="name"
-        autoComplete="name"
-        autoFocus
-      />
-      <TextField
-        margin="dense"
-        size="small"
-        required
-        fullWidth
-        id="nim"
-        label="NIM"
-        name="nim"
-      />
-      <TextField
-        margin="dense"
-        size="small"
-        required
-        fullWidth
-        name="password"
-        label="Kata Sandi"
-        type="password"
-        id="password"
-        autoComplete="new-password"
-      />
-      <TextField
-        margin="dense"
-        size="small"
-        required
-        fullWidth
-        name="password-confirmation"
-        label="Konfirmasi Kata Sandi"
-        type="password"
-        id="password-confirmation"
-        autoComplete="new-password"
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        sx={{ mt: 3 }}
-        component={RouterLink}
-        to="/mahasiswa/dashboard"
+export const MahasiswaSignUpForm = () => {
+  const navigate = useNavigate();
+  const [{ loading: registrationLoading }, registerMahasiswa] = useAxios(
+    {
+      url: "mahasiswa/register",
+      method: "POST",
+    },
+    { manual: true }
+  );
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const registrationResponse = await registerMahasiswa({
+      data: new FormData(event.target),
+    });
+    localStorage.setItem(
+      "mahasiswa_token",
+      registrationResponse.data.data.token
+    );
+    navigate("/mahasiswa/dashboard");
+  };
+
+  return (
+    <>
+      <Typography>Mahasiswa</Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{ textAlign: "center" }}
       >
-        Daftar
-      </Button>
-      <Container sx={{ mt: 2 }}>
-        <Link component={RouterLink} to="..">
-          Login
-        </Link>
-      </Container>
-    </Box>
-  </>
-);
+        <TextField
+          margin="dense"
+          size="small"
+          required
+          fullWidth
+          id="name"
+          label="Nama Lengkap"
+          name="name"
+          autoComplete="name"
+          autoFocus
+        />
+        <TextField
+          margin="dense"
+          size="small"
+          required
+          fullWidth
+          id="nim"
+          label="NIM"
+          name="nim"
+        />
+        <TextField
+          margin="dense"
+          size="small"
+          required
+          fullWidth
+          name="password"
+          label="Kata Sandi"
+          type="password"
+          id="password"
+          autoComplete="new-password"
+        />
+        <TextField
+          margin="dense"
+          size="small"
+          required
+          fullWidth
+          name="password-confirmation"
+          label="Konfirmasi Kata Sandi"
+          type="password"
+          id="password-confirmation"
+          autoComplete="new-password"
+        />
+        <LoadingButton
+          loading={registrationLoading}
+          type="submit"
+          variant="contained"
+          sx={{ mt: 3 }}
+        >
+          Daftar
+        </LoadingButton>
+        <Container sx={{ mt: 2 }}>
+          <Link component={RouterLink} to="..">
+            Login
+          </Link>
+        </Container>
+      </Box>
+    </>
+  );
+};
