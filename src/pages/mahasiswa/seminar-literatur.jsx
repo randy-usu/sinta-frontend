2af372c16ffb2121ee6ec842df5af806bbe41fc9
Randy H. <>
2024-07-22 02:36:59 +0700

    Implement mahasiswa seminar literatur index

--- src/pages/mahasiswa/seminar-literatur.jsx
+++ src/pages/mahasiswa/seminar-literatur.jsx
@@ -1,63 +1,45 @@
 import {
-  ArrowDropDown as ArrowDropDownIcon,
   Add as AddIcon,
-  Edit as EditIcon,
-  Delete as DeleteIcon,
   RemoveOutlined as RemoveIcon,
 } from "@mui/icons-material";
-
-import { useMemo, useState } from 'react';
-import React from "react";
-
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-} from 'material-react-table';
-
-import { useRef } from "react";
 import {
   Box,
   Button,
-  ButtonGroup,
-  ClickAwayListener,
+  Chip,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
   Fab,
-  Grow,
-  MenuList,
-  MenuItem,
-  Paper,
-  Popper,
-  styled,
-  useMediaQuery,
-  useTheme,
-  Typography,
   Grid,
   Stack,
-} from '@mui/material';
-
-import { data } from "../../features/layout/components/tabel-mahasiswa/tabel-seminar-literatur";
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import useAxios from "axios-hooks";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import React, { Fragment, useState } from "react";
 import Literatur from "../../features/layout/components/literatur/literatur";
 
-const VisuallyHiddenInput = styled('input')`
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: 1px,
-  overflow: 'hidden',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
-`;
-
 export default function SeminarLiteratur() {
+  const [
+    {
+      data: seminarLiteraturResponseData,
+      loading: seminarLiteraturRequestLoading,
+    },
+    refetchSeminarLiteratur,
+  ] = useAxios({
+    url: "mahasiswa/seminar-literatur",
+  });
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -68,193 +50,157 @@ export default function SeminarLiteratur() {
   };
 
   const [document, setDocument] = useState(["1"]);
-  const [documentNames, setDocumentNames] = useState(['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']);
-  function addComponent() { 
-    if (documentNames.length > 0) { 
+  const [documentNames, setDocumentNames] = useState([
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+  ]);
+  function addComponent() {
+    if (documentNames.length > 0) {
       setDocument([...document, documentNames[0]]);
       documentNames.splice(0, 1);
-    };
-  };
+    }
+  }
 
   const handleDeleteInput = (index) => {
     const newArray = [...document];
     newArray.splice(index, 1);
     setDocument(newArray);
   };
-  
-  const [action, setAction] = React.useState(false);
-  const anchorRef = React.useRef(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
 
-  const listItems = ['Edit', 'Hapus'];
-  
-  const handleOpenAction = () => {
-    window.alert(`You clicked ${listItems[selectedIndex]}`);
-  };
-
-  const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setAction(false);
-  };
-
-  const handleToggle = () => {
-    setAction((prevOpen) => !prevOpen);
-  };
-
-  const handleCloseAction = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-    setAction(false);
-  };
-
-  const columns = useMemo(
-    () => [
+  const columns = [
     {
-      accessorKey: 'status',
+      accessorKey: "status_text",
       header: "Status",
+      Cell: ({ cell, row }) => {
+        const STATUS_COLOR_MAP = {
+          proposed: "warning",
+          declined: "error",
+          approve: "success",
+        };
+
+        return (
+          <Chip
+            color={STATUS_COLOR_MAP[row.original.status]}
+            label={cell.getValue()}
+            variant="contained"
+          />
+        );
+      },
     },
     {
-      accessorKey: 'tanggal',
-      header: 'Tanggal',
-      filterVariant: 'text',
+      accessorFn: (dataRow) => new Date(dataRow.date),
+      header: "Tanggal",
+      filterVariant: "date-range",
+      Cell: ({ cell }) => cell.getValue().toLocaleDateString("id"),
     },
     {
-      accessorKey: 'nama_pic',
-      header: 'Nama PIC',
-      filterVariant: 'text'
+      accessorKey: "pic",
+      header: "Nama PIC",
+      filterVariant: "text",
     },
-    {
-      id: 'aksi',
-      header: 'Aksi',
-      Cell: () => (
-        <Box>
-          <div>
-            <ButtonGroup
-              variant="contained"
-              color="primary"
-              ref={anchorRef}>
-              <Button onClick={handleOpenAction}>{listItems[selectedIndex]}</Button>
+  ];
+
+  const table = useMaterialReactTable({
+    columns,
+    data: seminarLiteraturResponseData?.data ?? [],
+    enableGlobalFilter: false,
+    enableSorting: false,
+    enableColumnFilters: false,
+    state: {
+      isLoading: seminarLiteraturRequestLoading,
+    },
+  });
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, background: "#fafafa" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Stack direction="row">
+              <Typography component="h1" variant="h4" sx={{ flex: 1 }}>
+                Seminar Literatur
+              </Typography>
               <Button
-                size="small"
-                onClick={handleToggle}>
-                <ArrowDropDownIcon />
+                variant="contained"
+                endIcon={<AddIcon />}
+                onClick={handleClickOpen}
+                sx={{ borderRadius: 5, color: "black", marginBottom: 5 }}
+                color="inherit"
+              >
+                Ajukan
               </Button>
-            </ButtonGroup>
-            <Popper
-              sx={{
-                zIndex: 1,
-              }}
-              transition
-              open={action}
-              anchorEl={anchorRef.current}>
-              {({ TransitionProps }) => (
-              <Grow
-                {...TransitionProps}
-              >
-                <div style={{backgroundColor: 'green', color: 'white'}}>
-                  <Paper>
-                  <ClickAwayListener onClickAway={handleCloseAction}>
-                    <MenuList id="split-button-menu">
-                      {listItems.map((item, i) => (
-                        <MenuItem
-                          key={item}
-                          disabled={i === 2}
-                          selected={i === selectedIndex}
-                          onClick={(e) => handleMenuItemClick(e, i)}>
-                          {item}
-                        </MenuItem>
-                      ))}
-                    </MenuList>
-                  </ClickAwayListener>
-                  </Paper>
-                </div>
-              </Grow>
-              )}
-            </Popper>
-          </div>
-        </Box>
-      ),
-    }
-  ],
-);
-
-const table = useMaterialReactTable({
-  columns,
-  data,
-  initialState: {
-    showColumnFilter: true, 
-    showGlobalFilter: true,
-  },
-  positionGlobalFilter: "left",
-});
-
-return (
-  <>
-    <Box sx={{ flexGrow: 1, background: '#fafafa' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Stack direction="row">
-            <Typography component="h1" variant="h4" sx={{ flex: 1 }}>
-              Seminar Literatur
-            </Typography>
-            <Button
-              variant="contained"
-              endIcon={<AddIcon />}
-              onClick={handleClickOpen}
-              sx={{ borderRadius: 5, color: 'black', marginBottom: 5 }}
-              color="inherit"
-              positionActionsColumn="last"
-            >
-              Ajukan
-            </Button>
-            <Dialog
-              fullScreen={fullScreen}
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="responsive-dialog-title"
-              >
-              <DialogTitle id="responsive-dialog-title" align="center">
-                {"Formulir Pengusulan Seminar Literatur"}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  File Power Point
-                </DialogContentText>
-              <Button
-                component="label"
-                role="undefined"
-                variant="outlined"
-                tabIndex={-1}
-              >
-                <VisuallyHiddenInput type="file" />
-              </Button> 
-                {document.map((item, i) => (
-                <div>
-                  <DialogContentText>
-                    Literatur {i+1}
-                  </DialogContentText> 
-                  <Literatur text={item} /> 
-                  </div>
-                ))}
+              <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
+                <DialogTitle align="center">
+                  Formulir Pengusulan Seminar Literatur
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText>File Power Point</DialogContentText>
+                  <TextField
+                    required
+                    type="file"
+                    size="small"
+                    inputProps={{ accept: ".ppt,.pptx" }}
+                    // name="check_in_proof"
+                  />
+                  {document.map((item, i) => (
+                    <Fragment key={i}>
+                      <DialogContentText>Literatur {i + 1}</DialogContentText>
+                      <TextField
+                        required
+                        type="file"
+                        size="small"
+                        // inputProps={{ accept: ".ppt,.pptx" }}
+                        // name="check_in_proof"
+                      />
+                    </Fragment>
+                  ))}
                   <div>
-                    <Fab onClick={addComponent} size="small" color="info" aria-label="add"><AddIcon/></Fab>
-                    <Fab onClick={handleDeleteInput} size="small" color="error" aria-label="add"><RemoveIcon/></Fab>
+                    <Fab
+                      onClick={addComponent}
+                      size="small"
+                      color="info"
+                      aria-label="add"
+                    >
+                      <AddIcon />
+                    </Fab>
+                    <Fab
+                      onClick={handleDeleteInput}
+                      size="small"
+                      color="error"
+                      aria-label="add"
+                    >
+                      <RemoveIcon />
+                    </Fab>
                   </div>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button autoFocus variant="contained" onClick={handleClose}>
-                      OK
-                    </Button>
-                  </DialogActions>
-            </Dialog>
-          </Stack>
-            <div>
-              <MaterialReactTable table={table}></MaterialReactTable>
-            </div>
+                </DialogContent>
+                <DialogActions>
+                  <Button autoFocus variant="contained" onClick={handleClose}>
+                    OK
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </Stack>
+            <MaterialReactTable table={table} />
           </Grid>
-      </Grid>
-    </Box>
-  </>
-)
+        </Grid>
+      </Box>
+    </>
+  );
 }
